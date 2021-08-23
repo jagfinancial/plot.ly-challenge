dd825a385cd9cd4cd663720d02d74f7664362abf
jagfinancial <80197227+jagfinancial@users.noreply.github.com>
2021-08-22 19:07:48 -0700

    final commit

--- Belly_Button_Biodiversity/StarterCode/static/js/app.js
+++ Belly_Button_Biodiversity/StarterCode/static/js/app.js
@@ -1,12 +1,9 @@
-//Use the D3 library to read in samples.json.
+//readsamples.json.
 function buildPlot(sample) {
     d3.json("samples.json").then((data) => {
         
 // Build plot variable for samples.jason
         var samples = data.samples;
-
-// Use d3 to   set  location for bar  chart
-        var  barLocation = d3.select("#bar");
 
 // Filter data for object with corresponding sample number
         var resultArray = samples.filter(sampleObject => sampleObject.id == sample);
@@ -15,12 +12,15 @@ function buildPlot(sample) {
         var otu_labels = result.otu_labels
         sample_values=result.sample_values;
 
+// Use d3 to   set  location for bar  chart
+var  barLocation = d3.select("#bar");
+
 // Build horizontal bar chart
         var yticks = otu_ids.slice(0,10).map(outID => `OTU ${outID}`).reverse();
         var values = sample_values.slice(0,10).reverse();
         var labels = otu_labels.slice(0,10).reverse();
 
-// Create the horizantal bar trace
+
         var barData = [{
             y: yticks,
             x: values,
@@ -39,14 +39,14 @@ function buildPlot(sample) {
             }
         };
 
-// Plot horizontal bar chart
+ 
         Plotly.newPlot("bar", barData, barLayout, barLocation);
 
 //  Build the Bubble Chart
         d3.json("samples.json").then((data) => {
             var bubble_loc = d3.select("#bubble")
 
-// Create bubble chart trace
+
             var bubbleData = [
                 {
                     x: otu_ids,
@@ -76,16 +76,16 @@ function buildPlot(sample) {
         });
 
     })}
-// Display the individual demographic information - sample metadata
-// Display key-value pair from metadata json object somewhere  on the page
+ 
+// Bonus: Build Gauge Chart
 
 function buildMetada(sample) {
 
-// Get metadata from samples.json
+ 
     d3.json("samples.json").then((data) => {
         var meta_data = data.metadata;
 
-// Filter and use d3.select to assign metadata side panell variablle
+ 
 
         var resultArray = meta_data.filter(sampleObject => sampleObject.id == sample);
         var result = resultArray[0];
@@ -96,45 +96,17 @@ function buildMetada(sample) {
 
         }
         );
-//  Build Gauge Chart
 
-
-        var guageData = [
-            {
-                domain: { x: [0,1], y: [0,1]},
-                value: data.wfreq,
-                title: "Belly Button Washing Frequency Scrubs Per week",
-                type: "indicator",
-                mode: "gauge+number+delta",
-                delta: {reference:9, increasing: {color: "green"}},
-                gauge: {
-                    axis: {range: [0,10]},
-                    steps: [
-                        {range: [0,5], color: "lightgray"},
-                        {range: [5,8], color: "gray"}
-                    ],
-                    threshold: {
-                        line: {color: "red", width: 4},
-                        thickness: 0.75,
- },
-                       value: 9
-                    }
-                
-            }
-        ];
-
-        var guageLayout = {width: 600, height: 450, margin: {t: 0, b:0}};
-        Plotly.newPlot("gauge", guageData, guageLayout);
     }
     );
 }
 
-// Get reference to dropdown  select element
+ 
 function init() {
     var dropdownMenu = d3.select("#selDataset");
    
 
-// Loop through samples.json and add `option`, text and property for sample value  to populate select options
+ 
     d3.json("samples.json").then((data) => {
         var sampleNames = data.names;
         
@@ -145,20 +117,20 @@ function init() {
             .property("value", sample);
     });
 
-// Build default/initial plots using first sample from the list
+ 
     defaultSample = sampleNames[0];
     buildPlot(defaultSample);
     buildMetada(defaultSample);
 
 });
 }
-// Create function for change event
+ 
 
 function optionChanged(newSample) {
 
-// Get new data each time a new sample is selected
+ 
     buildPlot(newSample);
     buildMetada(newSample);
 }
-// Initialize dashboard !!!
+ 
 init();
